2a8b161490468164f27d510e223835db98caeb15
mrdog233o5 <mr.dog233o5@gmail.com>
2023-12-09 23:35:18 +0800

    homemade tilt effect :)

--- demo1/dynamicIsland.jsx
+++ demo1/dynamicIsland.jsx
@@ -2,10 +2,53 @@ var nav = document.getElementsByClassName("nav")[0];
 var content = nav.getElementsByClassName("content")[0];
 var computedContent;
 var contentLen;
-function frame() {
-    computedContent = getComputedStyle(content);
-    contentLen = content.clientWidth;
-    nav.style.width = `${contentLen}px`;
-    window.requestAnimationFrame(frame);
+var isMouseOver = false;
+var centerX;
+var centerY;
+var mouseX;
+var mouseY;
+
+function resetStyle() {
+    nav.style.transform = "";
+    nav.style.height = "0px";
 }
-window.requestAnimationFrame(frame);
+
+function tiltEffect() {
+	if (isMouseOver) {
+		console.log(centerX, centerY);
+		console.log(mouseX, mouseY);
+        const deltaScale = 15
+        var transformPhrase = `translate(${ ( mouseX - centerX ) / deltaScale }px, ${ ( mouseY - centerY ) / deltaScale }px)`;
+        console.log(transformPhrase);
+        nav.style.transform = transformPhrase; 
+		requestAnimationFrame(tiltEffect);
+	}
+}
+
+function sizeControl() {
+	computedContent = getComputedStyle(content);
+	contentLen = content.clientWidth;
+	nav.style.width = `${contentLen}px`;
+	window.requestAnimationFrame(sizeControl);
+}
+window.requestAnimationFrame(sizeControl);
+
+nav.addEventListener("mouseenter", () => {
+	isMouseOver = true;
+    nav.style.height = "150px";
+	var rect = nav.getBoundingClientRect();
+	centerX = Math.round(rect.left + rect.width / 2);
+    centerY = Math.round(rect.top + rect.height / 2);
+	requestAnimationFrame(tiltEffect);
+});
+
+nav.addEventListener("mouseleave", () => {
+	isMouseOver = false;
+    nav.style.height = "0px";
+    nav.style.transform = ""; 
+});
+
+document.addEventListener("mousemove", function(event) {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+});
--- demo1/test.jsx
+++ demo1/test.jsx
@@ -1,1 +1,6 @@
-
+/*
+var h1 = content.getElementsByClassName("h1")[0];
+setInterval(() => {
+    h1.innerHTML+="iii"
+}, 1000);
+*/
